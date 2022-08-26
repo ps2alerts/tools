d29927c6b0a731359647c54922fd2a11a6dac366
Matt Cavanagh <maelstrome26@gmail.com>
2022-08-26 15:15:41 +0100

    Fixed colours on the formatter

--- alert-sim/service/LoggingFormatter.py
+++ alert-sim/service/LoggingFormatter.py
@@ -2,16 +2,17 @@ import logging
 
 class LoggingFormatter(logging.Formatter):
 
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
+    grey = "\033[37m"
+    green = "\033[32m"
+    yellow = "\033[93m"
+    red = "\033[31m"
+    bold_red = "\033[31;1m"
+    reset = "\033[0m"
     format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
 
     FORMATS = {
         logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
+        logging.INFO: green + format + reset,
         logging.WARNING: yellow + format + reset,
         logging.ERROR: red + format + reset,
         logging.CRITICAL: bold_red + format + reset
