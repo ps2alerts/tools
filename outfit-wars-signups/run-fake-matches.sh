f22b545ff8251b2fa4051bd66e61edecb017f8e3
Matt Cavanagh <maelstrome26@gmail.com>
2022-08-28 23:12:58 +0100

    Wrapped rake matches in a shell

---
+++ outfit-wars-signups/run-fake-matches.sh
@@ -0,0 +1,10 @@
+if [[ ! -d .venv ]]; then
+    echo "Preparing virtual environment..."
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+else
+    source .venv/bin/activate
+fi
+
+python rake_matches.yp
